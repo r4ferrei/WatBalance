f100d4977260a581fae0dc225474af35df76dc89
Renato Ferreira <renato.wolp@gmail.com>
2015-02-17 19:29:36 -0500

    Fetching history table from watcard website

--- watbalance/routes/index.js
+++ watbalance/routes/index.js
@@ -1,12 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var needle = require('needle');
+var cheerio = require('cheerio');
 
 router.get('/', function(req, res, next) {
-  res.render('index', {content: 'home'});
+	res.render('index', { content: 'home', contentIsFile: true });
 });
 
-// router.post('/fetch', function(req, res, next) {
-// 
-// });
+router.post('/fetch', function(req, res, next) {
+	needle.post('https://account.watcard.uwaterloo.ca/watgopher661.asp',
+	{
+		acnt_1: req.body.number,
+		acnt_2: req.body.pin,
+		DBDATE: '02/15/2015',
+		DEDATE: '02/17/2015',
+		PASS: 'PASS',
+		STATUS: 'HIST'
+	}, function(err, response) {
+		if (!err && response.statusCode == 200) {
+			$ = cheerio.load(response.body);
+			res.render('index', {
+				content: $.html('#oneweb_financial_history_table'),
+				contentIsFile: false
+			});
+		}
+		else
+			res.send('Error: ' + response.statusCode);
+	});
+});
 
 module.exports = router;
