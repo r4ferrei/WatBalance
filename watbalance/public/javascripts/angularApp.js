c4b04a46ee97c85fbb1ebb87022305f8572d58dd
Renato Ferreira <renato.wolp@gmail.com>
2015-02-19 19:34:31 -0500

    Passing JSON to Angular with current term's expenses

--- watbalance/public/javascripts/angularApp.js
+++ watbalance/public/javascripts/angularApp.js
@@ -1,1 +1,10 @@
 var app = angular.module('watbalance', ['ui.router']);
+
+app.controller('FetchController', [
+	'$scope',
+	'table_json',
+	function($scope, table_json) {
+		$scope.table_json = table_json;
+		console.log($scope.table_json);
+	}
+]);
--- watbalance/routes/index.js
+++ watbalance/routes/index.js
@@ -4,27 +4,87 @@ var needle = require('needle');
 var cheerio = require('cheerio');
 
 router.get('/', function(req, res, next) {
-	res.render('index', { content: 'home', contentIsFile: true });
+	res.render('index', {
+		content: 'home',
+		contentIsFile: true
+	});
 });
 
 router.post('/fetch', function(req, res, next) {
-	needle.post('https://account.watcard.uwaterloo.ca/watgopher661.asp',
-	{
+	formatDate = function(date) {
+		return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+	}
+
+	calcStartDate = function() {
+		var date = new Date();
+		date.setDate(1);
+
+		if (date.getMonth() <= 3) // up to March - winter
+			date.setMonth(0);
+		else if (date.getMonth() <= 7) // up to August - spring
+			date.setMonth(4);
+		else // fall
+			date.setMonth(8);
+
+		return formatDate(date);
+	}
+
+	calcEndDate = function() {
+		return formatDate(new Date());
+	}
+
+	needle.post('https://account.watcard.uwaterloo.ca/watgopher661.asp', {
 		acnt_1: req.body.number,
 		acnt_2: req.body.pin,
-		DBDATE: '02/15/2015',
-		DEDATE: '02/17/2015',
+		DBDATE: calcStartDate(),
+		DEDATE: calcEndDate(),
 		PASS: 'PASS',
 		STATUS: 'HIST'
 	}, function(err, response) {
+		buildTableJSON = function($, selectors) {
+			table = $('#oneweb_financial_history_table');
+
+			if (table.length != 1)
+				return {};
+
+			// column indices with requested headers
+			cols = [];
+			for (var i = 0; i < selectors.length; i++)
+				cols.push(table.find(selectors[i]).index());
+
+			// get respective td's
+			ans = [];
+			table.children('tr').each(function(index) {
+				if ($(this).children('td').length == 0)
+					return;
+
+				// actual columns in this row
+				var row = {};
+				for (var i = 0; i < selectors.length; i++)
+					row[selectors[i]] = $($(this).children().get(cols[i])).html().trim();
+
+				ans.push(row);
+			});
+
+			return ans;
+		}
+
 		if (!err && response.statusCode == 200) {
 			$ = cheerio.load(response.body);
+
+			var table_json = buildTableJSON($, [
+				'#oneweb_financial_history_th_date',
+				'#oneweb_financial_history_th_amount',
+				'#oneweb_financial_history_th_bal'
+			]);
+			var table_param = JSON.stringify(table_json);
+
 			res.render('index', {
-				content: $.html('#oneweb_financial_history_table'),
+				content: '\
+					<div ng-controller="FetchController"><script> app.constant("table_json", ' + table_param + ') </script> </div>',
 				contentIsFile: false
 			});
-		}
-		else
+		} else
 			res.send('Error: ' + response.statusCode);
 	});
 });
